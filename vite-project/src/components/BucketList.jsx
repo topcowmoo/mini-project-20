ca8e3fc69797ddf96253972ea6f773b356c9d09a
Salvatore <mammoliti.sam@gmail.com>
2024-04-10 11:31:01 -0400

    refactored code

--- vite-project/src/components/BucketList.jsx
+++ vite-project/src/components/BucketList.jsx
@@ -1,58 +1,56 @@
-import { useState } from 'react';
-import BucketForm from './BucketForm';
-import Bucket from './Bucket';
+import { useState } from "react";
+import BucketForm from "./BucketForm";
+import Bucket from "./Bucket";
 
 function BucketList() {
-    const [bucket, setBucket] = (item) => {
-        console.log(
-            'File: BucketList.js ~ line 10 ~ addBucketItem ~ item',
-            item
-        );
-        if (!item.text) {
-            return;
-        }
+  const [bucket, setBucket] = useState([]);
 
-        const newBucket = [item, ...bucket];
-        console.log(newBucket);
+  // Function to add a bucket list item
+  const addBucketItem = (item) => {
+    console.log("File: BucketList.js ~ line 10 ~ addBucketItem ~ item", item);
+    // Check to see if the item text is empty
+    if (!item.text) {
+      return;
+    }
 
-        setBucket(newBucket);
-    };
+    // Add the new bucket list item to the existing array of objects
+    const newBucket = [item, ...bucket];
+    console.log(newBucket);
 
-    const completeBucketItem = (id) => {
-        let updatedBucket = bucket.map((item) => {
-            if (item.id === id) {
-                item.isComplete = !item.isComplete;
-            }
-            return item;
+    // Call setBucket to update state with our new set of bucket list items
+    setBucket(newBucket);
+  };
+
+  // Function to mark bucket list item as complete
+  const completeBucketItem = (id) => {
+    // If the ID passed to this function matches the ID of the item that was clicked, mark it as complete
+    let updatedBucket = bucket.map((item) => {
+      if (item.id === id) {
+        item.isComplete = !item.isComplete;
+      }
+      return item;
     });
 
     console.log(updatedBucket);
     setBucket(updatedBucket);
-};
+  };
 
-const completeBucketItem = (id) => {
-    let updatedBucket = bucket.map((item) => {
-        if (item.id === id) {
-            item.isComplete = !item.isComplete;
-        }
-        return item;
-    });
+  // Function to remove bucket list item and update state
+  const removeBucketItem = (id) => {
+    const updatedBucket = [...bucket].filter((item) => item.id !== id);
 
-    const removeBucketItem = (id) => {
-        const updatedBucket = [...bucket].filter((item) => item.id !== id);
+    setBucket(updatedBucket);
+  };
 
-        setBucket(updatedBucket);
-    };
-
-
-};
-
-const editBucketItem = (itemId, newValue) => {
+  // Function to edit the bucket list item
+  const editBucketItem = (itemId, newValue) => {
     // Make sure that the value isn't empty
     if (!newValue.text) {
       return;
     }
 
+    // We use the "prev" argument provided with the useState hook to map through our list of items
+    // We then check to see if the item ID matches the if of the item that was clicked and if so we set it to a new value
     setBucket((prev) =>
       prev.map((item) => (item.id === itemId ? newValue : item))
     );
